60b2ea2eed8e46c334ac294b60cd5e02ec68b136
TruFriendsNeverDie <heliodev@outlook.com>
2024-01-02 14:42:55 +0100

    Added a bit more functionality and cleaner code.
    The website isn't much yet and to fix the funny loading / reloading delay per card I will have to make it so each card runs it's own update.
    We'll see how that goes in the future.
    
    For now this is just a simple project where I tested querying the web.
    
    Enjoy!
    
    PS. I haven't tested responsiveness on mobile devices.

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 //Use fetch
 //try catch
 
-const fact = async () => {
-    const outputCard = document.getElementById('fact');
+createCard = () => {
+  c = document.createElement("div");
+  c.appendChild(document.createElement("h2"));
+  document.getElementsByClassName("factCards")[0].appendChild(c);
+  c.classList.add("fact");
+};
+
+const fact = async (Card) => {
+  createCard();
+  for (card in Card) {
+    const outputCard = Card[card].firstChild;
     try {
-        const factGET = await fetch("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en", {
-            method: "GET"
-        })
-        .then(factGET => factGET.json())
-        .then(factGET => factGET.text);
-        
-        console.log(factGET);
-        outputCard.innerHTML = factGET;
-    } catch (error) {
-        console.log(error);
-        if (error.message === "Failed to fetch") {
-            outputCard.innerHTML = "Failed to fetch (Website might be down at the moment)";
+      const factGET = await fetch(
+        "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en",
+        {
+          method: "GET",
         }
+      )
+        .then((factGET) => factGET.json())
+        .then((factGET) => factGET.text);
+
+      console.log(factGET);
+      outputCard.innerHTML = factGET;
+    } catch (error) {
+      console.log(error);
+      if (error.message === "Failed to fetch") {
+        outputCard.innerHTML =
+          "Failed to fetch (Website might be down at the moment)";
+      }
     }
-}
+  }
+};
 
-
-//add event listener to the button in the html file. the button has id of "btn" and it should call for the function fact
-
-
-document.getElementById("btn").addEventListener("click", fact);
+//add event listener to the button in the html ile. the button has id of "btn" and it should call for the function fact
+document.addEventListener(
+  "click",
+  fact.bind(this, document.getElementsByClassName("fact"))
+);
